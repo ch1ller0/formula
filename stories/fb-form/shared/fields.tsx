e57de2419dd32815e94c5adce95bd0fccc9c875b
Grigory Gorshkov <petralmazov100@gmail.com>
2021-06-21 21:36:14 +0300

    #19 Switch to React Suite Components (#21)

--- stories/fb-form/shared/fields.tsx
+++ stories/fb-form/shared/fields.tsx
@@ -1,38 +1,62 @@
-import { Label, Input, Select, Radio, Checkbox } from '@rebass/forms';
-import { Button, Box, Flex, Text as RText } from 'rebass';
-import { BaseSyntheticEvent } from 'react';
+import {
+  Button,
+  Input,
+  SelectPicker,
+  RadioGroup,
+  Radio,
+  Checkbox,
+  FlexboxGrid,
+} from 'rsuite';
 import { ViewGenerator } from '@formula/core';
+import 'rsuite/dist/styles/rsuite-default.css';
 
-const Text = (props: any) => (
-  <RText
-    fontFamily={'system-ui'}
-    fontSize={props.fontSize || 3}
-    fontWeight="normal"
-    color="black"
-    {...props}
+const Text: React.FC<
+  Partial<{
+    fontSize: number;
+    color: string;
+  }>
+> = (props) => (
+  <p
+    style={{
+      fontFamily: 'system-ui',
+      fontSize: (props.fontSize || 7) * 3,
+      fontWeight: 'normal',
+      color: props.color || 'black',
+      marginBottom: 6,
+    }}
   >
     {props.children}
-  </RText>
+  </p>
 );
 
-const Boxify: React.FC<{ error?: string }> = ({ children, error }) => {
+const Boxify: React.FC<{ error?: string; name?: string; label?: string }> = ({
+  children,
+  error,
+  label,
+  name,
+}) => {
   return (
     <>
-      <Box
-        p={5}
-        fontSize={2}
-        width={[1, 1, 1 / 2]}
-        sx={{
-          margin: [0, 1, 2],
-        }}
-      >
-        {children}
-        {error ? (
-          <Text fontSize={1} color="red">
-            {error}
-          </Text>
-        ) : null}
-      </Box>
+      <FlexboxGrid justify="start">
+        <FlexboxGrid.Item
+          colspan={24}
+          style={{
+            marginTop: 15,
+          }}
+        >
+          {label && name && (
+            <label htmlFor={name}>
+              <Text>{label}</Text>
+            </label>
+          )}
+          {children}
+          {error ? (
+            <Text fontSize={5} color="red">
+              {error}
+            </Text>
+          ) : null}
+        </FlexboxGrid.Item>
+      </FlexboxGrid>
     </>
   );
 };
@@ -42,20 +66,22 @@ export const InputFieldView = ViewGenerator.field<{
   error: string;
 }>({
   name: 'input',
-  initialValue: '',
+  initialValue: () => '',
   render: ({ value, setValue, name, label, error }) => {
-    const onChange = (
-      e: BaseSyntheticEvent<unknown, unknown, { value: string }>,
-    ) => setValue(e.target.value);
+    const onChange = (value: string) => {
+      setValue(value);
+    };
 
     return (
-      <Boxify error={error}>
-        {label && (
-          <Label htmlFor={name}>
-            <Text>{label}</Text>
-          </Label>
-        )}
-        <Input id={name} name={name} value={value} onChange={onChange} />
+      <Boxify error={error} name={name} label={label}>
+        <Input
+          id={name}
+          name={name}
+          // @TODO infer type instead of TPrimitive
+          value={value}
+          onChange={onChange}
+          size="lg"
+        />
       </Boxify>
     );
   },
@@ -69,28 +95,23 @@ export const SelectFieldView = ViewGenerator.field<{
   name: 'select',
   initialValue: ({ options }) => options[0].value,
   render: ({ value, setValue, label, options, error, name }) => {
-    const onChange = (
-      e: BaseSyntheticEvent<unknown, unknown, { value: string }>,
-    ) => {
-      const value =
-        options.find((v) => v.label === e.target.value)?.value ||
-        options[0].value;
+    const onChange = (value: string) => {
       setValue(value);
     };
-    const selectLabel = options.find((v) => v.value === value)?.label;
 
     return (
-      <Boxify error={error}>
-        {label && (
-          <Label htmlFor={name}>
-            <Text>{label}</Text>
-          </Label>
-        )}
-        <Select id={name} name={name} value={selectLabel} onChange={onChange}>
-          {options.map(({ label }) => (
-            <option key={label}>{label}</option>
-          ))}
-        </Select>
+      <Boxify error={error} label={label} name={name}>
+        <SelectPicker
+          data={options}
+          block
+          searchable={false}
+          cleanable={false}
+          size="lg"
+          id={name}
+          name={name}
+          value={value}
+          onChange={onChange}
+        />
       </Boxify>
     );
   },
@@ -104,33 +125,25 @@ export const RadioFieldView = ViewGenerator.field<{
   name: 'radio',
   initialValue: ({ options }) => options[0].value,
   render: ({ value: selectedValue, setValue, label, options, name, error }) => {
-    // @TODO The field is broken and only emits value once due to
-    // lack of rebass support of react controlled components
-    const onChange = (e: BaseSyntheticEvent<{ value: string }>) => {
-      setValue(e.target.value);
+    const onChange = (value: string) => {
+      setValue(value);
     };
 
-    const renderedOptions = options.map(({ value, label }, index) => (
-      <Label width={[1 / 2, 1 / 4]} p={2} key={value}>
-        <Radio
-          id={value}
-          name={name}
-          value={value}
-          onChange={onChange}
-          checked={value === selectedValue || index === 0}
-        />
-        <Text fontSize={2}>{label}</Text>
-      </Label>
-    ));
-
     return (
-      <Boxify error={error}>
-        {label && (
-          <Label htmlFor={name}>
-            <Text>{label}</Text>
-          </Label>
-        )}
-        <Flex>{renderedOptions}</Flex>
+      <Boxify error={error} label={label} name={name}>
+        <RadioGroup
+          appearance="default"
+          name="radioList"
+          inline
+          onChange={onChange}
+          value={selectedValue}
+        >
+          {options.map((a) => (
+            <Radio key={a.value} value={a.value}>
+              <Text fontSize={5}>{a.label}</Text>
+            </Radio>
+          ))}
+        </RadioGroup>
       </Boxify>
     );
   },
@@ -141,18 +154,26 @@ export const CheckboxFieldView = ViewGenerator.field<{
   error: string;
 }>({
   name: 'checkbox',
-  initialValue: false,
+  initialValue: () => false,
   render: ({ value, setValue, name, label, error }) => {
-    const onChange = (e: BaseSyntheticEvent<{ value: string }>) => {
-      setValue(e.target.checked);
+    const onChange = (_, value: boolean) => {
+      setValue(value);
     };
 
     return (
       <Boxify error={error}>
-        <Label width={[1 / 2]} p={2}>
-          <Checkbox id={name} name={name} onChange={onChange} checked={value} />
-          <Text fontSize={2}>{label}</Text>
-        </Label>
+        <Checkbox
+          style={{
+            marginLeft: 0,
+          }}
+          id={name}
+          name={name}
+          onChange={onChange}
+          checked={value}
+          inline
+        >
+          <Text fontSize={5}>{label}</Text>
+        </Checkbox>
       </Boxify>
     );
   },
@@ -163,7 +184,7 @@ export const SubmitButtonView = ViewGenerator.field<{
   disabled: boolean;
 }>({
   name: 'submit',
-  initialValue: null,
+  initialValue: () => null,
   render: ({ setValue, label, disabled }) => {
     const onClick = () => {
       setValue(null);
@@ -172,14 +193,11 @@ export const SubmitButtonView = ViewGenerator.field<{
     return (
       <Boxify>
         <Button
+          appearance={disabled ? 'subtle' : 'primary'}
           onClick={onClick}
-          // disabled={disabled}
-          bg={disabled ? 'disconnect' : 'primary'}
-          style={{
-            cursor: 'pointer',
-          }}
+          size="lg"
         >
-          <Text color={disabled ? 'white' : 'black'}>{label}</Text>
+          {label}
         </Button>
       </Boxify>
     );
@@ -191,20 +209,16 @@ export const ThankYouView = ViewGenerator.field<{
   link: { href: string; label: string };
 }>({
   name: 'final-page',
-  initialValue: null,
+  initialValue: () => null,
   render: ({ title, link: { href, label } }) => (
     <Boxify>
-      <Label width={[1]} p={2}>
-        <Text>{title}</Text>
-      </Label>
-      <Label width={[1]} p={2}>
-        <Text>
-          {label}
-          <a href={href} target="_blank">
-            {href}
-          </a>
-        </Text>
-      </Label>
+      <Text>{title}</Text>
+      <Text>
+        {label}
+        <a href={href} target="_blank">
+          {href}
+        </a>
+      </Text>
     </Boxify>
   ),
 });
